dd66c078456023ba3914c2ee35f38a83a0905915
Sung Kim <sungmkim80@gmail.com>
2018-09-16 16:32:41 -0400

    static import to take a screenshot

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "antd";
 
-if (process.env.NODE_ENV === "production") {
-  import("./styles.css");
-} else {
-  // Make sure that the custom stylesheet overrides antd css.
-  import("antd/dist/antd.css").then(() => import("./styles.css"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   import("./styles.css");
+// } else {
+//   // Make sure that the custom stylesheet overrides antd css.
+//   import("antd/dist/antd.css").then(() => import("./styles.css"));
+// }
+import "antd/dist/antd.css";
+import "./styles.css";
 console.log(`process.env.NODE_ENV=${process.env.NODE_ENV}`);
 
 function App() {
